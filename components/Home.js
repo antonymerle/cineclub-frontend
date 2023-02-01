65315809ac7df88b6dae87dfc880c47256cc0248
Antony Merle <antony.merle@gmail.com>
2023-02-01 11:18:11 +0100

    update fetch url production

--- components/Home.js
+++ components/Home.js
@@ -11,7 +11,7 @@ function Home() {
   const [moviesData, setMoviesData] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:3000/movies")
+    fetch("https://cineclub-jcbzwbtgv-antonymerle.vercel.app/movies")
       .then((res) => res.json())
       .then((data) => setMoviesData(data.results));
   }, []);
